c967e366581bebddeb614df09d1a3fc4c28520b0
sreejithsundaram <sreejithsundaram@outlook.com>
2025-07-30 13:23:18 +0530

    added exclamation

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask! This was added"
+    return "Hello, Flask!!!!! This was added"
 
 if __name__ == '__main__':
     app.run(debug=True)
